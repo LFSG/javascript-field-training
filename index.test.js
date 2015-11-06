97966904baa09e3835594cb149731f8645c227af
William Gottschalk <will.gottschalk@gmail.com>
2015-11-06 11:59:11 -0800

    removed demo files

--- index.test.js
+++ index.test.js
@@ -1,49 +1,72 @@
-var expect = require('chai').expect;
-var should = require('chai').should()
-// var homePage = require('./server');
+var expect = chai.expect;
+var should = chai.should()
 var assert = require('assert');
-var	http = require('http');
 var sinon = require('sinon');
 
-var server = require('./server');
+var server;
 
-describe('the server', function () {
-  it('should return 200', function (done) {
-    http.get('http://localhost:80', function (res) {
-      assert.equal(200, res.statusCode);
-      done();
-    });
-  });
-
-  it('should say "Hello, world!"', function (done) {
-    http.get('http://localhost:80', function (res) {
-      var data = '';
-      res.on('data', function (chunk) {
-        data += chunk;
-      });
-      res.on('end', function () {
-        res.body.should.be.a('string');
-        done();
-      });
-    });
-  });
-
-  it('should get data w code 200', function (done) {
-  	chai.request(server)
-        .get('/index.html') 
-        .end(function (err, res){
-          res.should.have.status(500);
-         
-        })
-  		
-  		done();
-  
-  });
-
-  it('should post data', function (done) {
-  	http.get('http://localhost:80', function (res) {
-  		done();
-  	})
-  });
-
+before(function () {
+  server = sinon.fakeServer.create();
 });
+
+after(function () {
+  server.restore();
+});
+
+it('should create a server that can listen for requests', function() {
+  var callback = sinon.spy();
+  getTodos(42, callback);
+
+  server.requests[0].respond( 200,
+    JSON.stringify([{ id: 1, test: "Provides examples", done: true}])
+  );
+
+  assert(callback.calledOnce);
+});
+
+
+
+
+
+
+
+// describe('the server', function () {
+//   it('should return 200', function (done) {
+//     http.get('http://localhost:80', function (res) {
+//       assert.equal(200, res.statusCode);
+//       done();
+//     });
+//   });
+
+//   it('should say "Hello, world!"', function (done) {
+//     http.get('http://localhost:80', function (res) {
+//       var data = '';
+//       res.on('data', function (chunk) {
+//         data += chunk;
+//       });
+//       res.on('end', function () {
+//         res.body.should.be.a('string');
+//         done();
+//       });
+//     });
+//   });
+
+//   it('should get data w code 200', function (done) {
+//   	chai.request(server)
+//         .get('/index.html') 
+//         .end(function (err, res){
+//           res.should.have.status(500);
+         
+//         })
+  		
+//   		done();
+  
+//   });
+
+//   it('should post data', function (done) {
+//   	http.get('http://localhost:80', function (res) {
+//   		done();
+//   	})
+//   });
+
+// });
--- sampleproject.js
+++
@@ -1,6 +0,0 @@
-// write a function that takes a name and says hello to the console.
-
-function hello (name) {
-	if (!name) name = 'World';
-	return `Hello ${name}!`;
-};
