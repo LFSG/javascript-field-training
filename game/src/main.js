0abffc5fa76a080e538cce75e0842fec918f1938
Brian <bgrober@gmail.com>
2015-11-06 14:05:22 -0800

    game try 2, still not moving

---
+++ game/src/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(e) {
+  var token = new Token($('#container'));
+  $('#container').on('keydown', function(e) {
+    if(e.keyCode === 37) {
+      token.direction = 'left';
+    }
+    if (e.keyCode === 39) {
+      token.direction = 'right';
+    }
+  });
+});
