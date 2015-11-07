f18467e957a3c8524d38987d1c04b76e2bf34738
James B <jamesaburnell@gmail.com>
2015-11-07 12:18:47 -0800

    game done

--- game/src/asteroids.js
+++ game/src/asteroids.js
@@ -1,7 +1,7 @@
 function AddAsteroid($el) {
 	this.node = $('<img class="asteroid" id="asteroid1" src="./pictures/asteroid.png"></img>');
 	this.SPEED = Math.floor(Math.random()*50);
-	this.node.css({top: 0, left: Math.floor(Math.random() * 14) * 50 + 8});
+	this.node.css({top: 0, left: Math.floor(Math.random() * 650) + 8});
 	$el.append(this.node);
 	setTimeout(this.move.bind(this), this.SPEED);
 }
@@ -15,13 +15,45 @@ AddAsteroid.prototype.move = function () {
 
 	if(position.top > 550) {
 		this.node.remove();
-		var score = $('#score').text();
-		score++;
-		$('#score').text(score);
+		// var score = $('#score').text();
+		// score++;
+		// $('#score').text(score);
 	}
 
-	if(ship.position().left - position.left < 50 && ship.position().left - position.left > -50 && ship.position().top - position.top < 50 && ship.position().top - position.top > -50){
+	if(ship.position().left - position.left < 45 && ship.position().left - position.left > -45 && ship.position().top - position.top < 45 && ship.position().top - position.top > -45){
     alert('game over');
 		location.reload();
   }
 };
+
+
+function AddBonusToken($el) {
+	this.node = $('<img class="bonusToken" src="./pictures/cs.png"></img>')
+	this.SPEED = Math.floor(Math.random()*20);
+	this.node.css({top: Math.floor(Math.random() * 250) + 8, left: Math.floor(Math.random() * 650) + 8});
+	$el.append(this.node);
+	setTimeout(this.move.bind(this), this.SPEED);
+}
+
+AddBonusToken.prototype.move = function () {
+	var ship = $('#ship');
+	var position = this.node.position();
+	position.top += 1;
+	this.node.offset(position);
+	setTimeout(this.move.bind(this), this.SPEED);
+
+	if(ship.position().left - position.left < 50 && ship.position().left - position.left > -50 && ship.position().top - position.top < 50 && ship.position().top - position.top > -50){
+    	var score = +$('#score').text();
+		score ++;
+		$('#score').text(score);
+		this.node.remove();
+    }
+
+  	if(position.top > 550) {
+  		var score = $('#score').text();
+		score --;
+		$('#score').text(score);
+		this.node.remove();
+	}
+
+}
