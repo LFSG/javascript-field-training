903197edbc98d01acb75ea7a238dafdf2cf05019
William Gottschalk <will.gottschalk@gmail.com>
2015-11-06 19:14:14 -0800

    working on front end tests

--- index.test.js
+++ index.test.js
@@ -13,60 +13,56 @@ after(function () {
   server.restore();
 });
 
-it('should create a server that can listen for requests', function() {
-  var callback = sinon.spy();
-  getTodos(42, callback);
 
-  server.requests[0].respond( 200,
-    JSON.stringify([{ id: 1, test: "Provides examples", done: true}])
-  );
+describe('the server', function () {
+  it('should create a server that can listen for requests', function() {
+      var callback = sinon.spy();
+      getTodos(42, callback);
 
-  assert(callback.calledOnce);
-});
+      server.requests[0].respond( 200,
+        JSON.stringify([{ id: 1, test: "Provides examples", done: true}])
+      );
 
+      assert(callback.calledOnce);
+    });
 
+  it('should return 200', function (done) {
+    http.get('http://localhost:80', function (res) {
+      assert.equal(200, res.statusCode);
+      done();
+    });
 
+  });
 
+  it('should say "Hello, world!"', function (done) {
+    http.get('http://localhost:80', function (res) {
+      var data = '';
+      res.on('data', function (chunk) {
+        data += chunk;
+      });
+      res.on('end', function () {
+        res.body.should.be.a('string');
+        done();
+      });
+    });
+  });
 
-
-
-// describe('the server', function () {
-//   it('should return 200', function (done) {
-//     http.get('http://localhost:80', function (res) {
-//       assert.equal(200, res.statusCode);
-//       done();
-//     });
-//   });
-
-//   it('should say "Hello, world!"', function (done) {
-//     http.get('http://localhost:80', function (res) {
-//       var data = '';
-//       res.on('data', function (chunk) {
-//         data += chunk;
-//       });
-//       res.on('end', function () {
-//         res.body.should.be.a('string');
-//         done();
-//       });
-//     });
-//   });
-
-//   it('should get data w code 200', function (done) {
-//   	chai.request(server)
-//         .get('/index.html') 
-//         .end(function (err, res){
-//           res.should.have.status(500);
+  it('should get data w code 200', function (done) {
+  	chai.request(server)
+        .get('/index.html') 
+        .end(function (err, res){
+          res.should.have.status(500);
          
-//         })
+        })
   		
-//   		done();
+  		done();
   
-//   });
+  });
 
-//   it('should post data', function (done) {
-//   	http.get('http://localhost:80', function (res) {
-//   		done();
-//   	})
-//   });
+  it('should post data', function (done) {
+  	http.get('http://localhost:80', function (res) {
+  		done();
+  	})
+  });
 
-// });
+});
